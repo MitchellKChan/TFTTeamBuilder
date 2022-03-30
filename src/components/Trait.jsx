34ae4df33e27c496761df84dbc246df339d489ae
Mitchell Chan <mitchellkchan>
2022-03-29 18:41:46 -0700

    added styling to Trait Component and added spacing to Item Components inside a BoardHex Component

--- src/components/Trait.jsx
+++ src/components/Trait.jsx
@@ -8,9 +8,14 @@ function Trait(props) {
         nextBreakpoint = props.traitInfo.sets[traitLevel++];
     }
     return (
-        <div className="trait">
-            <p>{props.traitInfo.name}: {props.count} / {nextBreakpoint.min}</p>
-            <img src={props.iconPath} draggable="false" alt={props.name} data-toggle="tooltip" title={props.name}></img>
+        <div className="trait p-2 my-3">
+            <div className="d-inline-block align-middle px-1">
+                <img src={props.iconPath} draggable="false" alt={props.name} data-toggle="tooltip" title={props.name}></img>
+            </div>
+            <div className="d-inline-block align-middle px-1">
+                <span>{props.traitInfo.name}</span>
+                <div>{props.count} / {nextBreakpoint.min}</div>
+            </div>
         </div>
     );
 }
